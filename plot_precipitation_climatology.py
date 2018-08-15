0bc54eb8d2fbf69207cd61cc4a7131a9fdf55551
shree <shreeram.iof@gmail.com>
2018-08-15 15:50:41 -0400

    updated adding masking function

--- plot_precipitation_climatology.py
+++ plot_precipitation_climatology.py
@@ -23,14 +23,26 @@ def read_data(fname, month):
 
 def convert_pr_units(cube):
     """Convert kg m-2 s-1 to mm day-1"""
-    
+    assert cube.units == 'kg m-2 s-1', "Program assumes that input units are kg m-2 s-1"
     cube.data = cube.data * 86400
     cube.units = 'mm/day'
     
     return cube
 
+def apply_mask(pr_cube, sftlf_cube, realm):
+    """Mask ocean using a sftlf (land surface fraction) file."""
+   
+    if realm == 'land':
+        mask = numpy.where(sftlf_cube.data > 50, True, False)
+    else:
+        mask = numpy.where(sftlf_cube.data < 50, True, False)
+   
+    pr_cube.data = numpy.ma.asarray(pr_cube.data)
+    pr_cube.data.mask = mask
+   
+    return pr_cube
 
-def plot_data(cube, month, gridlines=False):
+def plot_data(cube, month, gridlines=False, levels=None):
     """Plot the data."""
         
     fig = plt.figure(figsize=[12,5])    
@@ -47,7 +59,6 @@ def plot_data(cube, month, gridlines=False):
     title = '%s precipitation climatology (%s)' %(cube.attributes['model_id'], month)
     plt.title(title)
 
-
 def main(inargs):
     """Run the program."""
 
@@ -55,18 +66,31 @@ def main(inargs):
     pdb.set_trace()
     cube = convert_pr_units(cube)
     clim = cube.collapsed('time', iris.analysis.MEAN)
-    plot_data(clim, inargs.month)
+    if inargs.mask:
+        sftlf_file, realm = inargs.mask
+        sftlf_cube = iris.load_cube(sftlf_file, 'land_area_fraction')
+        clim = apply_mask(clim, sftlf_cube, realm)
+    
+    plot_data(clim, inargs.month, gridlines=inargs.gridlines,
+              levels=inargs.cbar_levels)
     plt.savefig(inargs.outfile)
 
 
 if __name__ == '__main__':
+
     description='Plot the precipitation climatology for a given month.'
     parser = argparse.ArgumentParser(description=description)
     
     parser.add_argument("infile", type=str, help="Input file name")
-    parser.add_argument("month", type=str, help="Month to plot")
+    parser.add_argument("month", type=str, choices=calendar.month_abbr[1:], help="Month to plot")
     parser.add_argument("outfile", type=str, help="Output file name")
 
-    args = parser.parse_args()
-    
+    parser.add_argument("--gridlines", action="store_true", default=False,
+                        help="Include gridlines on the plot")
+    parser.add_argument("--cbar_levels", type=float, nargs='*', default=None,
+                        help='list of levels / tick marks to appear on the colourbar')
+    parser.add_argument("--mask", type=str, nargs=2, metavar=('SFTLF_FILE', 'REALM'), default=None,
+                        help='Apply a land or ocean mask (specify the realm to mask: "land" or "ocean")')
+
+    args = parser.parse_args()            
     main(args)
